89e61f07f54301d606ac48355ffa1368ab51a7a4
Ewan Brain <ewan.brain@gmail.com>
2021-01-12 23:00:01 +0000

    API fetch working

--- src/app.js
+++ src/app.js
@@ -3,5 +3,30 @@ import Vue from 'vue';
 document.addEventListener("DOMContentLoaded", () => {
   new Vue({
     el: "#app",
+    data:{
+      exchangeRates: [], //this array now contains data taken from API with fetchExchangeRates method, which is mounted (run at start)
+      base: 0,
+      date: "",
+      selectedCurrency: 0
+    },
+    computed:{ 
+      }
+
+    },
+    mounted: function(){
+      this.fetchExchangeRates()
+      
+    },
+    methods:{
+      fetchExchangeRates: function(){
+        fetch("https://api.exchangeratesapi.io/latest")
+        .then(response => response.json())
+        .then(data => {
+          this.exchangeRates = data.rates
+          this.base = data.base;
+          this.date = data.date;
+        })
+      }
+    }
   })
 });
